f205ce2a9621b995bf61ae2b09228610c1bc808b
Habiba10 <84009417+Habiba10@users.noreply.github.com>
2021-11-14 01:28:23 +0500

    Add files via upload

---
+++ automatic_sanitizer_dispenser.ino
@@ -0,0 +1,31 @@
+#include <Servo.h>
+const int servoPin=3;
+const int trigPin=4;
+const int echoPin=5;
+long duration;
+int distance;
+Servo servo;
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin (9600);
+  servo.attach(servoPin);
+  servo.write(0);
+}
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration*(0.034/2);
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  if (distance < 10) {
+  servo.write(90);
+  Serial.println("Activated");
+  delay(2000);
+  servo.write(0);
+}
+  
+}
